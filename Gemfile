92dda819e2fa6d93b2cf89492e98ee7d9fb48a3e
O-Azmi <azmioualid@gmail.com>
2024-03-19 10:23:56 -0500

    db migrated and installed gems

--- Gemfile
+++ Gemfile
@@ -26,6 +26,23 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Add active admin gem
+gem "activeadmin"
+
+# Add active storage gem 
+gem "activestorage"
+
+# Add sassc gem
+gem "sassc"
+
+# Add faker gem
+gem "faker"
+
+
+
+
+
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
---
+++ db/schema.rb
@@ -0,0 +1,76 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_03_19_151452) do
+  create_table "authors", force: :cascade do |t|
+    t.string "author_name"
+    t.date "author_birthdate"
+    t.string "author_birthplace"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "book_name"
+    t.date "bok_date"
+    t.decimal "book_price"
+    t.text "book_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email_address"
+    t.string "address"
+    t.string "username"
+    t.string "password"
+    t.string "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "genre_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.decimal "quantity"
+    t.decimal "amount_after"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.date "order_date"
+    t.decimal "total_amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "province_name"
+    t.decimal "tax_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "publisher_name"
+    t.date "publication_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
